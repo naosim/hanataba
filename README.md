69ee97d887ac0b8bc44f420d7ecaacdcf8565a99
naosim <87138+naosim@users.noreply.github.com>
2023-12-24 21:12:06 +0900

    一旦コミット

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 [NPO法人 IT勉強宴会 - 花束問題V1.2](https://www.benkyoenkai.org/contents/Bouquet1-2)
 
 ```mermaid
-
+---
+title: Node
+---
+flowchart LR
+    id["hoge<hr />+ string name<br />+ string age<br />"]
+  
 ```
--- static/js/doc.mjs
+++ static/js/doc.mjs
@@ -2,6 +2,8 @@ import {PoccoIO} from "./PoccoIO.mjs"
 import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
 mermaid.initialize({startOnLoad: false});
 
+const rootPath = "/js/";
+
 (async function() {
   var poccoIO = new PoccoIO();
   // var obj = await poccoIO.read("data.json");
@@ -11,25 +13,42 @@ mermaid.initialize({startOnLoad: false});
     .filter(v => v.kind == "class")
     .map(v => new ClassDef(v));
   console.log(classDefs);
+  document.querySelector(".mermaid").innerHTML = toMermaidText(classDefs);
+  mermaid.init();
+})()
 
+/**
+ * 
+ * @param {ClassDef[]} classDefs 
+ */
+function toMermaidText(classDefs) {
   var text = "\nclassDiagram\n" + classDefs.map(v => {
+    const lines = [];
+    lines.push(`  namespace ${v.namespace} { class ${v.className} }`);
 
-    const lines = [`  namespace ${v.namespace} { class ${v.className} }`];
+    if(v.isAbstract) {
+      lines.push(`  <<Abstract>> ${v.className}`)
+    }
+
     // プロパティ
     v.properties.map(p => `  ${v.className} : +${p.type} ${p.propertyName}`).forEach(p => lines.push(p))
     
     // メソッド
-    v.methods.map(p => `  ${v.className} : +${p.returnType} ${p.methodName}()`).forEach(p => lines.push(p))
+    v.methods.map(p => `  ${v.className} : +${p.returnType} ${p.methodName}()${p.isStatic ? "$" : ""}`).forEach(p => lines.push(p))
 
     // 依存関係
     const ignoreTypes = new Set(["any", "string", "boolean", "number", "Date", "void"])
     var dependecies = new Set();
     v.properties
-      .filter(p => !ignoreTypes.has(p.type) )
-      .forEach(p => dependecies.add(p.type.split("[]").join("")));
+      .map(p => p.type.split("[]").join(""))
+      .filter(p => v.className != p)
+      .filter(p => !ignoreTypes.has(p))
+      .forEach(p => dependecies.add(p));
     v.methods
-      .filter(p => !ignoreTypes.has(p.returnType) )
-      .forEach(p => dependecies.add(p.returnType.split("[]").join("")));
+      .map(p => p.returnType.split("[]").join(""))
+      .filter(p => v.className != p)
+      .filter(p => !ignoreTypes.has(p))
+      .forEach(p => dependecies.add(p));
     dependecies.forEach(p => lines.push(`  ${v.className} --> ${p}`));
     
     lines.push("") // 見やすくするための空行
@@ -38,10 +57,34 @@ mermaid.initialize({startOnLoad: false});
   }).join("\n")
 
   text = text.split(">").join("&gt;").split("<").join("&lt;");
-  console.log(text);
-  document.querySelector(".mermaid").innerHTML = text;
-  mermaid.init();
-})()
+  return text;
+}
+
+/**
+ * 
+ * @param {ClassDef[]} classDefs 
+ */
+function toMermaidFlowchartText(classDefs) {
+  var text = "\nclassDiagram\n" + classDefs.map(v => {
+    const lines = [];
+    lines.push(`  namespace ${v.namespace} { class ${v.className} }`);
+
+    if(v.isAbstract) {
+      lines.push(`  <<Abstract>> ${v.className}`)
+    }
+
+    // プロパティ
+    v.properties.map(p => `  ${v.className} : +${p.type} ${p.propertyName}`).forEach(p => lines.push(p))
+    
+    // メソッド
+    v.methods.map(p => `  ${v.className} : +${p.returnType} ${p.methodName}()${p.isStatic ? "$" : ""}`).forEach(p => lines.push(p))
+    return lines.join("\n")
+  }).join("\n")
+
+  text = text.split(">").join("&gt;").split("<").join("&lt;");
+  return text;
+
+}
 
 class ClassDef {
   className
@@ -51,6 +94,7 @@ class ClassDef {
   location
   package
   org
+  isAbstract
   constructor(denoDocClassObj) {
     var properties = denoDocClassObj.classDef.properties.map(p => {
       // console.log(p.jsDoc);
@@ -59,6 +103,16 @@ class ClassDef {
         type: p.jsDoc?.tags.filter(t => t.kind == "type").map(t => t.type)[0] || "any"
       }
     })
+    /*
+    "jsDoc": {
+      "tags": [
+        {
+          "kind": "unsupported",
+          "value": "@abstract"
+        }
+      ]
+    }
+    */
 
     var methods = denoDocClassObj.classDef.methods.map(p => {
       /** @type {any[]} */
@@ -66,10 +120,12 @@ class ClassDef {
       var returnType = tags.filter(t => t.kind == "return").map(t => t.type)[0] || "void";
       // Promiseを消す。モデリングにおいて重要な情報でないため
       returnType = returnType.split("Promise<").join("").split(">").join("")
-      return {methodName: p.name, returnType}
+      return {methodName: p.name, returnType, isStatic: p.isStatic}
     })
 
     var location = decodeURI(denoDocClassObj.location.filename)
+
+    var isAbstract = denoDocClassObj.jsDoc?.tags?.filter(v => v.kind == "unsupported" && v.value == "@abstract").length > 0;
 
     this.className = denoDocClassObj.name,
     this.extends = denoDocClassObj.classDef.extends,
@@ -78,10 +134,14 @@ class ClassDef {
     this.package = location.split("/").at(-1),
     this.org = denoDocClassObj
     this.methods = methods
+    this.isAbstract = isAbstract
   }
   
   get namespace() {
-    return this.location.split("/").at(-1).split(".").join("_");
+    return this.location.split(rootPath)[1].split("/").at(-1).split(".").join("_");
+  }
+  get namespaces() {
+    return this.location.split(rootPath)[1].split("/").map(v => v.split(".").join("_"));
   }
 }
 
